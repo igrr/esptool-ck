cdaa43b1800dc9211638011a911d33ff51d915a4
Neil Kolban <kolban1@kolban.com>
2015-08-27 21:18:17 -0500

    Added the '-?' command flag
    Added the '-?' command flag to to display help information.

--- argparse/argparse.c
+++ argparse/argparse.c
@@ -63,6 +63,28 @@ int parse_arg(int num_args, char **arg_ptr)
                 return 1;
                 break;
 
+            case '?':
+            	INFO(\
+"esptool\n" \
+"-------\n" \
+"-eo <filename>	Opens an ELF object file, parses it and caches some of the information found therein. Only works if there is no ELF file currently opened.\n" \
+"-es <section> <filename> Reads the given section from the ELF file and makes a raw dump into the specified file.\n" \
+"-ec            Closes the currently opened ELF file.\n" \
+"-bo <filename> Prepares an firmware file in the format that is understood by the ESP chip. Only works if an ELF file is opened, and if no firmware file is prepared yet. Upon -bo the tool will start out with an empty image where only the main header is set up. The result of the operations done on the firmware image are saved when the it is finally closed using -bc command.\n" \
+"-bm <qio|qout|dio|dout> Set the flash chip interface mode. Default is QIO. This parameter is stored in the binary image header, along with the flash size and flash frequency. The ROM bootloader in the ESP8266 uses the value of these parameters in order to know how to talk to the flash chip.\n" \
+"-bz <512K|256K|1M|2M|4M|8M|16M|32M>	Set the flash chip size. Default is 512K.\n" \
+"-bf <40|26|20|80> Set the flash chip frequency, in MHz. Default is 40M.\n" \
+"-bs <section>  Reads the specified section from the ELF file and appends it to the firmware image. Sections will appear in the firmware image in the exact same order as the -bs commands are executed.\n" \
+"-bc            Closes the firmware image and saves the result as file to disk.\n" \
+"-v             Increase verbosity level of the tool. Add more v's to increase it even more, e.g. -vv, -vvv.\n" \
+"-q             Disable most of the output.\n" \
+"-cp <device>   Select the serial port device to use for communicating with the ESP. Default is /dev/ttyUSB0 on Linux, COM1 on Windows, /dev/tty.usbserial on Mac OS X.\n" \
+"-cd <board>    Select the reset method to use for resetting the board. Currently supported methods are listed below.\n" \
+"-cb <baudrate> Select the baudrate to use, default is 115200.\n" \
+"-ca <address>  Address in flash memory to upload the data to. This address is interpreted as hexadecimal. Default is 0x00000000.\n" \
+"-cf <filename> Upload the file to flash. Parameters that set the port, baud rate, and address must preceed the -cf command.\n");
+            	break;
+
             default:
                 return 0;
                 break;
